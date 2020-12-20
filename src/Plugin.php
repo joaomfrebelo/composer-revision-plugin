5fb3a6f6bf2c5b215be6a04d1180128816f3cfb8
João Rebelo <joaomfrebelo@gmail.com>
2020-12-20 15:26:52 +0000

    fix: implement missing methods required by PluginInterface of composer 2.0

--- src/Plugin.php
+++ src/Plugin.php
@@ -83,4 +83,12 @@ class Plugin implements PluginInterface, EventSubscriberInterface
         array_pop($verParts);
         return join(".", $verParts);
     }
+    
+    public function uninstall(Composer $composer, IOInterface $io){
+        
+    }
+    
+    public function deactivate(Composer $composer, IOInterface $io){
+        
+    }    
 }
